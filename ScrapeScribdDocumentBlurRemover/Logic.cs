3729698409c7c576dfca8968f202884f6cbf5cfa
alexander-sil <93575945+alexander-sil@users.noreply.github.com>
2022-09-03 10:04:39 +0300

    Add files via upload

---
+++ ScrapeScribdDocumentBlurRemover/Logic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+
+namespace ScrapeScribdDocumentBlurRemover
+{
+    public static class Logic
+    {
+        public static void Execute()
+        {
+
+        }
+
+        public static void ScrapeHTML()
+        {
+
+        }
+
+        public static string GetResponse(string url)
+        {
+            try
+            {
+                using (HttpClient http = new HttpClient())
+                {
+                    http.DefaultRequestHeaders.Add("User-Agent", "Twitterbot/1.0");
+                    HttpResponseMessage response = http.GetAsync(url).Result;
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("An exception has been thrown. " + e.Message);
+                        return "<p>TEST</p>";
+                    }
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception has been thrown. " + e.Message);
+                return "<p>TEST</p>";
+            }
+        }
+    }
+}
+
---
+++ ScrapeScribdDocumentBlurRemover/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ScrapeScribdDocumentBlurRemover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ;
+        }
+    }
+}
+
